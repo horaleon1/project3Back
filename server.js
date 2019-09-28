da70d73e3a892a23bd1b53ad7237ae729aa90444
horaleon1 <horaleon1@gmail.com>
2019-09-27 21:11:02 -0500

    "cors"

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 const app = express();
 
@@ -10,6 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req,res) =>  res.json({ msg: 'Welcome to my world' }));
+
+//middlewares
+app.use(cors());
 
 //Routes
 app.use('/users', require('./routes/users'));
