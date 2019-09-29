c22ee1997f9be09974d07c6c91ad5655147081dc
horaleon1 <horaleon1@gmail.com>
2019-09-29 17:00:14 -0500

    sign in

--- routes/auth.js
+++ routes/auth.js
@@ -49,7 +49,7 @@ router.post(
       const isMatch = await bcrypt.compare(password, user.password);
 
       if(!isMatch){
-        return res.status(400).json({ msg: 'Invalid credentials'})
+        return res.status(400).json({ msg: 'Invalid Credentials'})
       }
 
       const payload = {
