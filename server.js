e8ab70425f87124a29192491e14f7c9a745d3b1b
horaleon1 <horaleon1@gmail.com>
2019-10-01 19:44:24 -0500

    "cors 3rd"

--- server.js
+++ server.js
@@ -6,21 +6,27 @@ const app = express();
 
 //Connect Database
 connectDB();
+
+// const whitelist = [
+//   "https://blockchainp3f.herokuapp.com/",
+//   'http://blockchainp3f.herokuapp.com/'
+// ]
+
+//middlewares
+// app.use(cors({
+  
+
+
+// }));
+
+app.use(cors());
 //Init Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req,res) =>  res.json({ msg: 'Welcome to my world' }));
 
-//middlewares
-app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
+
 
 //Routes
 app.use('/users', require('./routes/users'));
