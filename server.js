fb4fffeb1124e418049b291fc9842b26a3636236
horaleon1 <horaleon1@gmail.com>
2019-10-13 22:55:33 -0500

    "FIX/ auth.js"

--- server.js
+++ server.js
@@ -1,47 +1,21 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
+// const path = require('path');
 
 const app = express();
 
 //Connect Database
 connectDB();
 
-// const whitelist = [
-//   "https://blockchainp3f.herokuapp.com/",
-//   'http://blockchainp3f.herokuapp.com/'
-// ]
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
-
-// app.use(cors());
-
-// app.use(cors({
-//   'allowedHeaders': ['sessionId', 'Content-Type'],
-//   'exposedHeaders': ['sessionId'],
-//   'origin': '*',
-//   'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   'preflightContinue': false
-//  }));
-
 // middlewares
 app.use(cors());
 
-// app.use(cors());
 //Init Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req,res) =>  res.json({ msg: 'Welcome to my world' }));
-
-
+app.get('/', (req,res) =>  res.json({ msg: 'Bienvenido a Mundo Blockchain' }));
 
 //Routes
 app.use('/users', require('./routes/users'));
